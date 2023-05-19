d618cd6d0b80ba8049bc36280712d7514d093e28
Ricky Sam <50199974+haroldejfsam@users.noreply.github.com>
2023-05-19 23:49:28 +0200

    Do js exercise 03
    Did js exercise 03 which deals with using a loop to reverse a string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    reversedWord = '';
+    for (let i= word.length-1;i>=0;i--) {
+        reversedWord += word.charAt(i)
+    }
+    return reversedWord
 };
 
 // Do not edit below this line
