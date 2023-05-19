e3dfe2be5049526c1cfb14ede99ab37e9d1bdb02
Ricky Sam <50199974+haroldejfsam@users.noreply.github.com>
2023-05-19 22:38:31 +0200

    Do js exercise 2
    Did js exercise 2. This deals with loops and testing with jest.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(a,b) {
+    if (b < 0) {
+        return "ERROR"
+    }
+    let string = "";
+    for (let i = 0; i < b; i++) {
+        string += a
+    }
+    return string;
 };
 
 // Do not edit below this line
