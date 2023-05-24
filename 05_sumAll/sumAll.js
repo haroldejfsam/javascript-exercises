156513a1eac16d2e5d588020bce77d900acaf0b2
Ricky Sam <50199974+haroldejfsam@users.noreply.github.com>
2023-05-25 00:19:35 +0200

    Do js exercise 05
    Did js exercise 05 which deals with data validation.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let finalSum = 0;
+
+    let smallerNum = (num1 < num2) ? num1:num2;
+    let biggerNum = (num2 > num1) ? num2:num1;
+    
+    if (typeof num1 != 'number' || typeof num2 != 'number') {
+        return 'ERROR'
+    }   else if (smallerNum<0 || biggerNum<0) {
+        return 'ERROR'
+    } else {
+    
+        for (let i = smallerNum; i <= biggerNum; i++) {
+            finalSum += i; 
+        
+        }
+    
+    return finalSum;
+        
+    }
 
 };
 
