3e5bd73616537fcbdca9a2dafa5422a726947a13
Ricky Sam <50199974+haroldejfsam@users.noreply.github.com>
2023-05-25 13:08:27 +0200

    Do js exercise 07
    Did js exercise 07 which deals with temperature conversions. Used
    toFixed function to round to 1 decimal point and Number method to
    convert string to number.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+//x °F ≘ (x − 32) × 5/9 °C
+let convertedToCelsius = (fahrenheit-32) * (5/9);
+
+return Number(convertedToCelsius.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+//x °C ≘ (x × 9/5 + 32) °F
+let convertedToFahrenheit = (celsius*(9/5)+32)
+
+return Number(convertedToFahrenheit.toFixed(1));
+
 };
 
 // Do not edit below this line
